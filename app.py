1fdb74805a168efbe55c433902d82cf94bdcd651
Elliott Tamarkin <tamarkinellie@gmail.com>
2024-07-19 17:27:24 +0000

    fixed all style changes

--- app.py
+++ app.py
@@ -21,13 +21,11 @@ app.secret_key = os.getenv('FLASK_SECRET_KEY')
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 GPT_API_KEY = os.getenv('GPT_API_KEY')
 
-
 # Route for home page
 @app.route('/')
 def home():
     session.clear()  # Clear session variables
     return render_template('home.html')
-
 
 # Route for login page
 @app.route('/login', methods=['GET', 'POST'])
@@ -42,7 +40,6 @@ def login():
         else:
             flash("Invalid username or password. Please try again.", "danger")
     return render_template('login.html')
-
 
 # Route for registration page
 @app.route('/register', methods=['GET', 'POST'])
@@ -64,7 +61,6 @@ def register():
             flash(error_message, "danger")
     return render_template('register.html')
 
-
 # Route for user dashboard
 @app.route('/dashboard')
 @login_required
@@ -79,13 +75,11 @@ def dashboard():
 
     return render_template('dashboard.html', user=user)
 
-
 # Route to access the features page
 @app.route('/discover')
 @login_required
 def discover():
     return render_template('discover.html')
-
 
 # Route for collecting information for the match the day feature
 @app.route('/match_the_day_info', methods=['GET', 'POST'])
@@ -99,11 +93,10 @@ def match_the_day_info():
         session['activity'] = activity  # Set activity in session
         session['genre'] = genre  # Set genre in session
 
-        #return redirect(url_for('song_matches'))
+        # Return redirect(url_for('song_matches'))
         songs, weather_stats = get_songs_from_activity(city, activity, genre)
 
         return render_template('song_matches.html', songs=songs, city=city, activity=activity, weather_stats=weather_stats)
-
 
     # Reset city, activity, and genre if navigating back
     session.pop('city', None)
@@ -111,7 +104,6 @@ def match_the_day_info():
     session.pop('genre', None)
     genres = get_spotify_genres()
     return render_template('match_the_day_info.html', genres=genres)
-
 
 # Route for collecting information for songs
 @app.route('/match_the_mood_info', methods=['GET', 'POST'])
@@ -125,7 +117,7 @@ def match_the_mood_info():
 
         songs, mood = get_songs_from_mood(mood, genre)
 
-        #print(songs)
+        # Print(songs)
         return render_template('song_matches.html', songs=songs, mood=mood)
 
     # Reset if navigating back
@@ -133,7 +125,6 @@ def match_the_mood_info():
     session.pop('genre', None)
     genres = get_spotify_genres()
     return render_template('match_the_mood_info.html', genres=genres)
-
 
 # Route for collecting information for songs
 @app.route('/match_the_song_info', methods=['GET', 'POST'])
@@ -152,8 +143,7 @@ def match_the_song_info():
     genres = get_spotify_genres()
     return render_template('match_the_song_info.html', genres=genres)
 
-
-@app.route('/song_matches', methods=['GET','POST'])  # redirect to dashboard
+@app.route('/song_matches', methods=['GET', 'POST'])  # redirect to dashboard
 @login_required
 def song_matches():  # make this reusable to display similar songs and mood songs and activity songs
     if request.method == 'POST':
@@ -165,9 +155,9 @@ def song_matches():  # make this reusable to display similar songs and mood song
 
         song_id = save_song(user_id, song_name, artist_name, album_name, song_link)
         if song_id:
-            return {"status":"success","message":"Song saved successfully"},200
+            return {"status": "success", "message": "Song saved successfully"}, 200
         else:
-            return {"status":"error","message":"Song failed to save. Please try again"}, 500
+            return {"status": "error", "message": "Song failed to save. Please try again"}, 500
 
     previous_page = session.get('previous_page')
     if previous_page == 'match_the_day_info':
@@ -186,7 +176,6 @@ def song_matches():  # make this reusable to display similar songs and mood song
         songs = get_similar_songs(original_song)
         return render_template('song_matches.html', songs=songs, original_song=original_song)
 
-
 # Route for saving a song
 @app.route('/save_a_song', methods=['POST'])
 def save_a_song():
@@ -196,13 +185,12 @@ def save_a_song():
         artist_name = request.form['artist_name']
         album_name = request.form['album_name']
         song_link = request.form['song_link']
-        
+
         song_id = save_song(user_id, song_name, artist_name, album_name, song_link)
         if song_id:
-            return {"status":"success","message":"Song saved successfully"},200
+            return {"status": "success", "message": "Song saved successfully"}, 200
         else:
-            return {"status":"error","message":"Song failed to save. Please try again"}, 500
-        
+            return {"status": "error", "message": "Song failed to save. Please try again"}, 500
 
 # Route for viewing saved songs
 @app.route('/saved_songs')
@@ -210,7 +198,6 @@ def saved_songs():
     user_id = session.get('user_id')
     saved_songs = get_saved_songs(user_id)
     return render_template('saved_songs.html', saved_songs=saved_songs)
-
 
 # Route for deleting a saved song
 @app.route('/delete_saved_song/<int:song_id>', methods=['POST'])
@@ -226,13 +213,11 @@ def delete_saved_song(song_id):
         flash("User not found.", "danger")
     return redirect(url_for('saved_songs'))
 
-
 # Route for logging out
 @app.route('/logout')
 def logout():
     session.clear()  # Clear all session variables
     return redirect(url_for('home'))
-
 
 # Run the Flask application
 if __name__ == '__main__':
